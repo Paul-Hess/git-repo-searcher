8b1074f83474e7f238d5ab74e061bae9e1ca7fb1
Paul-Hess <pauljacksonhess@gmail.com>
2016-03-18 11:12:11 -0700

    add ui function to return git owner avatar, username, links, to page

--- js/user-interface.js
+++ js/user-interface.js
@@ -3,26 +3,44 @@ var GetUser = require('./../js/scripts.js').GetUser;
 
 $(document).ready(function() {
 
-	// $('#form').submit(function(event) {
-	// 	event.preventDefault();
-	// 	var userQuery = $('#input').val();
-		var token = "?access_token=" + gitApiToken;
-		var newGitCall = new GetUser("paul-hess", token);
-		
+	$('#get-user').submit(function(event) {
+		event.preventDefault();
+		var userQuery = $('#user-query').val();
+		var token = "\?access_token=" + gitApiToken;
+		var newGitCall = new GetUser(userQuery, token);
+		console.log('foo');
 
 		$.get(newGitCall.request).then(function(response) {
-			console.log(response);
+			$('#avatar').empty();
+			$('#owner-info').empty();
+			var avatar = response[0].owner.avatar_url;
 			var name = response[0].name;
+			var owner = response[0].owner.login;
+			var gitLink = response[0].owner.html_url;
+			var gistUrl = response[0].gist_url;
+			console.log(response);
 			console.log(name);
 			console.log(response[0].description);
 			console.log(response[0].clone_url);
 			console.log(response.length);
-			// $('.landing').text(name);
-			// ('.landingd2').text();
+			if ( avatar !== " " && avatar !== "") {
+				$('#avatar').prepend('<img class="profile-img" src=' + avatar + '>');
+			} else {
+					$('#avatar').prepend('<img src="./../assets/img/git-default.png"' );
+			};
+
+			$('#owner-info').append('<h2>' + owner + "</h2>")
+			.append('<a href=' + gitLink + "> git page for " + owner + "</a>");
+
+			if (gistUrl !== " " && gistUrl !== "" && gistUrl !== undefined) {		
+				$('#owner-info').append('<a href=' + gistUrl  + "> gist page for " + owner + "</a>");
+			};
+			
+
 		}).fail(function(error) {
 			response = error.responseJSON.messsage;
 		}); //end get.
-	// }); //end submit.
+	}); //end submit.	
 
 }); //end doc ready
 
